6d0e6f9504a66aa47a93b827033b8d709394ffa8
none <none@yhaoo.com>
2024-02-27 12:27:48 +0100

    included js file

---
+++ assets/js/snackbar.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+    }

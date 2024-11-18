88e7c91a2cf84fea6ae2500036a3b1f0f04a530e
none <none@yhaoo.com>
2024-11-18 20:19:58 +0100

    create effect overflow cv

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,32 @@ iconSpan.forEach((span, index) => {
         span.style.justifyContent = 'start'; // Modifica il colore del testo per i paragrafi con indice dispari
     }
 });
+
+// Selezioniamo tutti gli elementi <p>
+const paragrafi = containerP.querySelectorAll('p');
+
+// Iteriamo su tutti gli elementi <p>
+paragrafi.forEach((paragrafo, index) => {
+    // Se l'indice è pari (0, 2, 4, ...), applica un cambiamento
+    if (index % 2 === 0) {
+        paragrafo.style.textAlign = 'left';  // Modifica il colore del testo per i paragrafi con indice pari
+        // Puoi aggiungere altre modifiche qui
+    } else {
+        paragrafo.style.textAlign = 'right'; // Modifica il colore del testo per i paragrafi con indice dispari
+    }
+});
+
+// quando si vede si sovrappone alle altre grid
+const overFlowCv = document.querySelector('.row-arrows-more');
+const showCv = document.querySelector('.grid-cv');
+
+    overFlowCv.addEventListener('click', function(event) {
+        console.log(event.target.tagName);
+        if (event.target.tagName === 'BUTTON') { 
+            if(event.target.id === "btn-down"){
+              showCv.style.overflow = "visible";  //bisogna mettergli l'onfocus quando si preme da un'altra parte
+            }
+        }else{
+            return;
+            }
+        });
